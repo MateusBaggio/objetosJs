86653c77b0910e9841ff7d51c252a4e3d180705a
Mateus Santos Baggio <167319374+MateusBaggio@users.noreply.github.com>
2024-08-28 09:35:50 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+// var n = new Number('23.45678')
+// console.log(n.toFixed(3));
+
+// var n = new Number('23.45678')
+// console.log(n.toPrecision(3));
+
+// var n = new Number('54.47849');
+// console.log(n.toExponential());
+// console.log(n.toExponential(5));
+// console.log(n.toExponential(3));
+
+// var x = new String('123456789');
+// console.log(x.length);
+
+// var y = new String('texto hoje');
+// console.log(y.charAt(8));
+
+// var e = new String('criando uma string');
+// console.log(e.charCodeAt(4));
+
+// var w = new String('criando uma string');
+// console.log(w.concat(' e concatenando com outra'));
+
+// var w = new String('criando uma string');
+// console.log(w.indexOf('uma'));
+
+// var w = new String('Teste criando uma string Teste');
+// console.log(w.lastIndexOf('Teste'));
+
+// var x = new String('Criando uma string')
+// var y = /i/;
+// console.log(x.match(y));
+
+// var a = /z/;
+// console.log(x.match(a));
+
+// var x = new String('Criando uma string');
+// console.log(x.replace('string', 'teste'));
+
+// var x = new String('Criando uma string');
+// console.log(x.substring(6, 15));
+
+// var x = new String('String, Criando uma string');
+// console.log(x.split(" "));
+
+// var x = new String('String, Criando uma string');
+// console.log(x.toLowerCase(x));
+// console.log(x.toUpperCase(x));
